ea921e8214211e6ea22cbecf155b0b6fb731b08e
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-12-14 05:59:03 -0800

    Add:analyser to load the state code information from a csv file

--- src/main/java/org/example/IndianCensusAnalyser.java
+++ src/main/java/org/example/IndianCensusAnalyser.java
@@ -9,27 +9,58 @@ import java.util.List;
 import com.opencsv.CSVReader;
 public class IndianCensusAnalyser {
     public static List<IndianCensusData> indianCensusList = new ArrayList<>();
+    public static List<StateCSV> stateCodeList = new ArrayList<>();
+    public static int i = 0;
 
-    public int loadCensusData(String filePath) throws Exception {
-        try {
+    public int loadCensusData(String filePath) throws Exception{
+        try{
             indianCensusList = new ArrayList<>();
             CSVReader reader = new CSVReader(new FileReader(filePath));
             List<String[]> data = reader.readAll();
-            data.stream().forEach(n -> {
+            data.stream().forEach(n->{
                 Iterator<String> iterate = Arrays.stream(n).iterator();
                 String state = iterate.next();
                 String population = iterate.next();
                 String areaInSqKm = iterate.next();
                 String densityPerSqKm = iterate.next();
-                if (!population.equals("Population"))
+                if(!population.equals("Population"))
                     indianCensusList.add(new IndianCensusData(state, Long.parseLong(population), Long.parseLong(areaInSqKm), Integer.parseInt(densityPerSqKm)));
             });
             reader.close();
-        } catch (FileNotFoundException e) {
-            throw new CustomException(e.getMessage(), CustomException.ExceptionType.File_Not_Found);
-        } catch (IllegalStateException e) {
-            throw new CustomException(e.getMessage(), CustomException.ExceptionType.Parse_Error);
+        }catch(FileNotFoundException e) {
+            throw new CustomException(e.getMessage(),CustomException.ExceptionType.File_Not_Found);
+        }
+        catch(IllegalStateException  e) {
+            throw new CustomException(e.getMessage(),CustomException.ExceptionType.Parse_Error);
         }
         return indianCensusList.size();
     }
+
+    public int loadStateData(String filePath) throws Exception{
+        try{
+            i = 0;
+            stateCodeList = new ArrayList<>();
+            CSVReader reader = new CSVReader(new FileReader(filePath));
+            List<String[]> data = reader.readAll();
+            data.stream().forEach(n->{
+                Iterator<String> iterate = Arrays.stream(n).iterator();
+                String srNo = iterate.next();
+                String state = iterate.next();
+                String TIN = iterate.next();
+                String stateCode = iterate.next();
+                if(i == 0)
+                    i=1;
+                else
+                    stateCodeList.add(new StateCSV(Integer.valueOf(srNo),state,Integer.valueOf(TIN),stateCode));
+            });
+            reader.close();
+        }catch(FileNotFoundException e) {
+            throw new CustomException(e.getMessage(),CustomException.ExceptionType.File_Not_Found);
+        }
+        catch(IllegalStateException  e) {
+            throw new CustomException(e.getMessage(),CustomException.ExceptionType.Parse_Error);
+        }
+        return stateCodeList.size();
+    }
+
 }
--- src/test/java/org/example/IndianCensusTest.java
+++ src/test/java/org/example/IndianCensusTest.java
@@ -2,13 +2,13 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class IndianCensusTest {
-    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\admin\\eclipse-workspace\\indiancensusanalyser\\src\\test\\java\\org\\example\\indiacensusdata.csv";
-    public IndianCensusAnalyser analyser;
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\Kunal\\IdeaProjects\\IndianStateCensus\\src\\test\\resources\\IndiaStateCensusData.csv";
+    IndianCensusAnalyser analyser=new IndianCensusAnalyser();
+    private static final String INDIAN_CSV_STATE_PATH = "C:\\Users\\Kunal\\IdeaProjects\\IndianStateCensus\\src\\test\\resources\\StateCode.csv";
 
     @Test
     public void returnSize() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -18,7 +18,6 @@ public class IndianCensusTest {
     @Test
     public void shouldExecuteProperly_IfRecordsAreEqual() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -28,7 +27,6 @@ public class IndianCensusTest {
     @Test
     public void shouldReturnRecordsif_CSVFileIsDetected() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -38,7 +36,6 @@ public class IndianCensusTest {
     @Test
     public void shouldReturnCustomException_ifDelimiterIncorrect() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -48,8 +45,57 @@ public class IndianCensusTest {
     @Test
     public void shouldReturCustomException_ifCSVHeaderIncorrect() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//	State
+
+    @Test
+    public void returnSizeState() throws Exception {
+        try {
+
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldExecuteProperly_IfRecordsAreEqualState() throws Exception {
+        try {
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldReturnRecordsif_CSVFileIsDetectedState() throws Exception {
+        try {
+            Assertions.assertEquals(37, analyser.loadStateData("xyz.csv"));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldReturnCustomException_ifDelimiterIncorrectState() throws Exception {
+        try {
+
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldRetureCustomException_ifCSVHeaderIncorrectState() throws Exception {
+        try {
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
         }
